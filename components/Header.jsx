a4b3800e766ff3d427f0004ea2337c4c994e5148
namja85 <namja85@gmail.com>
2022-05-16 23:23:21 +0900

    헤더, 푸터 추가

---
+++ components/Header.jsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <div className="sticky top-0 w-full border-b border-slate-50/[0.06] backdrop-blur">
+      <div className="max-w-7xl mx-auto">
+        <div className="mx-4 py-4 flex justify-between items-center">
+          <Link href={'/'}>
+            <a>
+              <h1 className="text-slate-200 font-bold text-3xl">PNJA C.</h1>
+            </a>
+          </Link>
+          <div>
+            <button className="text-slate-400 hover:cursor-pointer hover:text-slate-300">
+              <span className="sr-only">Navigation</span>
+              <svg width="24" height="24">
+                <path
+                  d="M5 6h14M5 12h14M5 18h14"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
