62045d4706aeea2ef32e21673c6e3dc189c175ee
namja85 <namja85@gmail.com>
2022-06-06 23:15:45 +0900

    IntroCard 이미지를 Image 컴포넌트로 교체

--- components/IntroCard.jsx
+++ components/IntroCard.jsx
@@ -1,9 +1,20 @@
+import Image from 'next/image';
+
 export default function IntroCard({ name, imgPath, text, bibleNum }) {
   return (
     <div className="relative rounded-lg text-slate-300">
       <div className="flex p-4 rounded-lg overflow-hidden bg-slate-800 ring-1 ring-slate-700">
-        <div className="w-32 -m-4 mr-4 h-auto overflow-hidden">
+        {/* <div className="w-32 -m-4 mr-4 h-auto overflow-hidden">
           <img className="object-cover" src={imgPath} alt={name} />
+        </div> */}
+        <div className="relative w-32 -m-4 mr-4 h-40 overflow-hidden">
+          <Image
+            src={imgPath}
+            alt={name}
+            layout="fill"
+            objectFit="cover"
+            priority
+          />
         </div>
         <div className="flex-1 flex items-center">
           <div className="space-y-2">
