cbafb225067f7040056fa17e71e299e4de46024c
namja85 <namja85@gmail.com>
2022-06-19 18:39:03 +0900

    Header max-width 변경

---
+++ components/ActiveLink.jsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import cn from 'classnames';
+
+export default function ActiveLink({ children, href }) {
+  const router = useRouter();
+  const active = router.asPath === href;
+  console.log(router, active);
+
+  return (
+    <Link href={href}>
+      <a>
+        <span
+          className={cn(
+            'px-8 py-2 hover:text-white hover:border-b-2 hover:border-b-sky-400',
+            { 'text-white border-b-2 border-b-sky-500': active },
+          )}
+        >
+          {children}
+        </span>
+      </a>
+    </Link>
+  );
+}
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,22 +1,33 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import MenuIcon from './MenuIcon';
+import ActiveLink from './ActiveLink';
 
 export default function Header() {
   return (
     <header className="sticky top-0 w-full border-b border-slate-50/[0.06] backdrop-blur">
-      <div className="max-w-lg mx-auto">
+      <div className="max-w-lg mx-auto md:max-w-3xl lg:max-w-5xl">
         <div className="mx-4 py-4 flex justify-between items-center">
           <Link className="cursor-pointer" href={'/'}>
             <a>
               <img className="h-8" src="/logo.png" alt="평내중앙교회로고" />
             </a>
           </Link>
-          <div>
+          <div className="lg:hidden">
             <button className="text-slate-400 hover:cursor-pointer hover:text-slate-300">
               <span className="sr-only">Navigation</span>
               <MenuIcon />
             </button>
+          </div>
+          <div className="hidden lg:block">
+            <ul className="flex space-x-2">
+              <li>
+                <ActiveLink href="/bulletins">주보</ActiveLink>
+              </li>
+              <li>
+                <ActiveLink href="/bibles">성경</ActiveLink>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
