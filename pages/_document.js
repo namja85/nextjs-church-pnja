ae296e345e8d35c69d97a87c8445ef44a622d2ae
GyeongeunHyun <gehyun@osstem.com>
2022-06-20 13:48:06 +0900

    robots.txt, sitemap.xml, favicon 링크 추가

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="https://www.pnja.or.kr/favicon.ico"></link>
+        <link rel="apple-touch-icon" href="/favicon.ico"></link>
+        <link rel="apple-touch-icon-precomposed" href="/favicon.ico"></link>
+      </Head>
       <body className="antialiased bg-slate-900 text-slate-400">
         <Main />
         <NextScript />
