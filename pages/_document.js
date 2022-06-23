ac48ae77b6632edfb869a7c63db074cf89cf20ed
namja85 <namja85@gmail.com>
2022-06-24 00:37:19 +0900

    카카오 공유 썸네일 이미지 및 애플터치아이콘 이미지 변경

--- pages/_document.js
+++ pages/_document.js
@@ -5,13 +5,13 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="shortcut icon" href="https://www.pnja.or.kr/favicon.ico"></link>
-        <link rel="apple-touch-icon" href="/favicon.ico"></link>
-        <link rel="apple-touch-icon-precomposed" href="/favicon.ico"></link>
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png"></link>
+        <link rel="apple-touch-icon-precomposed" href="/icons/apple-touch-icon.png"></link>
 
         <meta property="og:type" content="website"></meta>
         <meta property="og:title" content="평내중앙교회"></meta>
         <meta property="og:description" content="평내중앙교회에 오신 것을 환영합니다."></meta>
-        <meta property="og:image" content="https://www.pnja.or.kr/logo.png"></meta>
+        <meta property="og:image" content="https://www.pnja.or.kr/icons/thumbnail.png"></meta>
         <meta property="og:url" content="https://www.pnja.or.kr"></meta>
       </Head>
       <body className="antialiased bg-slate-900 text-slate-400">
