4a188f9753f551f117f32c8dadbf44d3bc8f26a8
GyeongeunHyun <gehyun@osstem.com>
2022-06-24 18:00:31 +0900

    canonical link 추가

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,14 +6,6 @@ import Footer from './Footer';
 export default function Layout({ children }) {
   return (
     <div className="min-h-screen min-w-[430px]">
-      <Head>
-        <title>평내중앙교회</title>
-        <meta
-          name="description"
-          content="평내중앙교회는 여러분을 환영합니다."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Header />
       <Main>{children}</Main>
       <Footer />
