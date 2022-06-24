7d4a733006a99cf61b3ac9a5306bd17e54e505da
namja85 <namja85@gmail.com>
2022-06-25 00:15:15 +0900

    아이콘 추가

--- pages/_document.js
+++ pages/_document.js
@@ -2,12 +2,17 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="ko">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
         <link rel="shortcut icon" href="https://www.pnja.or.kr/favicon.ico"></link>
-        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png"></link>
-        <link rel="apple-touch-icon-precomposed" href="/icons/apple-touch-icon.png"></link>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"></link>
+        <meta name="msapplication-TileColor" content="#2b5797"></meta>
+        <meta name="theme-color" content="#373737"></meta>
 
         <meta property="og:type" content="website"></meta>
         <meta property="og:title" content="평내중앙교회"></meta>
