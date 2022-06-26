4b9436249687a341ee89dd33729340f144beb06e
namja85 <namja85@gmail.com>
2022-06-26 20:55:01 +0900

    Hero Text 구조 변경

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,36 +1,30 @@
 export default function Hero({ children }) {
   return (
     <div className="pt-16 pb-32 px-16 lg:pt-32 lg:pb-48 lg:px-4">
-      <h1 className="flex flex-col items-center space-y-4 dark:text-slate-100 font-extrabold text-4xl lg:flex-row lg:space-y-0 lg:space-x-4 lg:justify-center">
-        <span>
-          <span className="font-bold text-sky-500 dark:text-sky-400 lg:text-6xl">
-            축제
-          </span>
-          가 있는 교회,
-        </span>
-        <span>
-          <span className="font-bold text-sky-500 dark:text-sky-400 lg:text-6xl">
-            생동
-          </span>
-          하는 교회,
-        </span>
-        <span>
-          <span className="font-bold text-sky-500 dark:text-sky-400 lg:text-6xl">
-            꿈
-          </span>
-          을 주는 교회
-        </span>
+      <h1 className="text-center leading-relaxed dark:text-slate-100 font-extrabold text-4xl">
+        <strong className="font-bold text-sky-500 dark:text-sky-400 lg:text-6xl">
+          축제
+        </strong>
+        가 있는 교회,<br className="lg:hidden"></br>
+        <strong className="font-bold text-sky-500 dark:text-sky-400 lg:text-6xl">
+          생동
+        </strong>
+        하는 교회,<br className="lg:hidden"></br>
+        <strong className="font-bold text-sky-500 dark:text-sky-400 lg:text-6xl">
+          꿈
+        </strong>
+        을 주는 교회
       </h1>
-      <p className="mt-6 text-center lg:mt-11">
-        <span className="font-medium text-sky-500 dark:text-sky-400">
+      <h2 className="mt-6 text-center lg:mt-11">
+        <em className="font-medium text-sky-500 dark:text-sky-400">
           하나님 중심
-        </span>
+        </em>
         으로 말씀과 은혜가{' '}
-        <span className="font-medium text-sky-500 dark:text-sky-400">
+        <em className="font-medium text-sky-500 dark:text-sky-400">
           풍성한 사랑
-        </span>
+        </em>
         의 공동체
-      </p>
+      </h2>
       <div className="mt-6 flex justify-center space-x-4 lg:mt-11">
         {children}
       </div>
