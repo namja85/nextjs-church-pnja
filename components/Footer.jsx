d222654daef18c3ff2a2ae5ea09a6750966f25d1
GyeongeunHyun <gehyun@osstem.com>
2023-03-21 13:16:10 +0900

    홈페이지 footer에 메일 정보 추가

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,23 @@
 import Image from 'next/image';
+import MailIcon from './MailIcon';
+import LocationIcon from './LocationIcon';
 
 export default function Footer() {
   return (
     <footer className="border-t border-slate-50/[0.06] py-10 px-4">
-      <div className="p-4 max-w-lg mx-auto flex justify-between items-center md:max-w-3xl lg:max-w-5xl">
+      <div className="p-4 max-w-lg mx-auto flex justify-between items-start md:max-w-3xl lg:max-w-5xl">
         <div>
-          <p>
+          <p className="space-x-1 flex items-center">
+            <LocationIcon />
             <small>(12223) 경기도 남양주시 평내로29번길 51-30</small>
           </p>
-          <p>
+          <p className="space-x-1 flex items-center">
+            <MailIcon />
+            <small>
+              <a href="mailto:info@pnja.or.kr">문의: info@pnja.or.kr</a>
+            </small>
+          </p>
+          <p className='mt-4'>
             <small>Copyright © 2022 평내중앙교회. All rights reserved.</small>
           </p>
         </div>
