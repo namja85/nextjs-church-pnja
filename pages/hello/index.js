546168446de7552f08bfcf548acc3fdcaa542af2
namja85 <namja85@gmail.com>
2023-12-08 06:28:47 +0900

    eslint 에러 수정

--- pages/hello/index.js
+++ pages/hello/index.js
@@ -17,14 +17,14 @@ export default function Hello() {
         </h1>
         <hr />
         <h2 className="text-center text-xl font-bold">
-          "세계선교를 통한 인재양성, 금융권을 통한 선교, 학원가를 통한 선교!"
+          &quot;세계선교를 통한 인재양성, 금융권을 통한 선교, 학원가를 통한 선교!&quot;
         </h2>
       </div>
 
       <div className="my-16 flex flex-col justify-center text-center space-y-4">
         <p>예수 그리스도의 이름으로 여러분을 환영합니다.</p>
-        <p>"평내중앙교회"는 지역의 복음화를 위한 다음세대를 준비하기 위하여</p>
-        <p>"세계선교를 통한 인재양성, 금융권을 통한 선교, 학원가를 통한 선교"라는 교회의 3대 Vision을 가지고 기도하며 실천하고 있습니다.</p>
+        <p>&quot;평내중앙교회&quot;는 지역의 복음화를 위한 다음세대를 준비하기 위하여</p>
+        <p>&quot;세계선교를 통한 인재양성, 금융권을 통한 선교, 학원가를 통한 선교&quot;라는 교회의 3대 Vision을 가지고 기도하며 실천하고 있습니다.</p>
         <p>하나님의 은혜 가운데 여러분과 이 일들을 이루어 나가고 싶습니다.</p>
         <p className="!mt-16 text-lg space-x-4"><span className="font-bold">담임목사</span><span>김명호</span></p>
       </div>
