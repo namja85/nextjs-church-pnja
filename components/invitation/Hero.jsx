9f0437e38bb1a2318ce12ab656125b92479caa7f
namja85 <namja85@gmail.com>
2024-08-22 22:15:41 +0900

    헤더 삭제

--- components/invitation/Hero.jsx
+++ components/invitation/Hero.jsx
@@ -6,27 +6,29 @@ export default function Hero() {
   const titleRef = useRef(null);
   const headerRef = useRef(null);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((e) => {
-        if (e.isIntersecting) {
-          headerRef.current.style.opacity = 0;
-        } else {
-          headerRef.current.style.opacity = 1;
-        }
-      });
-    });
-    observer.observe(titleRef.current);
-  }, []);
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver((entries) => {
+  //     entries.forEach((e) => {
+  //       if (e.isIntersecting) {
+  //         headerRef.current.style.opacity = 0;
+  //       } else {
+  //         headerRef.current.style.opacity = 1;
+  //       }
+  //     });
+  //   });
+  //   observer.observe(titleRef.current);
+  // }, []);
 
   return (
     <div className={styles.heroWrapper}>
-      <div
-        ref={headerRef}
-        className="opacity-0 fixed h-4 text-xs font-bold z-20 w-full text-center border-b shadow-md bg-gray-200 text-gray-600"
-      >
-        2024년도 임직감사예배 - 평내중앙교회
-      </div>
+      {false && (
+        <div
+          ref={headerRef}
+          className="opacity-0 fixed h-4 text-xs font-bold z-20 w-full text-center border-b shadow-md bg-gray-200 text-gray-600"
+        >
+          2024년도 임직감사예배 - 평내중앙교회
+        </div>
+      )}
       <div className="hero-inner p-8 relative bg-gray-50 w-full h-[1000px]">
         <div ref={titleRef} className={styles.title}>
           <h1>임직감사예배</h1>
