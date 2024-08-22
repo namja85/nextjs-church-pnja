4c18f45d54e82e480958914c4533093d7e686bdf
namja85 <namja85@smartscore.kr>
2024-08-22 17:07:17 +0900

    헤더 추가

--- components/invitation/Hero.jsx
+++ components/invitation/Hero.jsx
@@ -1,11 +1,35 @@
+import { useEffect, useRef } from 'react';
 import Image from 'next/image';
 import styles from '../../pages/notice/invitation.module.css';
 
 export default function Hero() {
+  const titleRef = useRef(null);
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((e) => {
+        console.log(e);
+        if (e.isIntersecting) {
+          headerRef.current.style.opacity = 0;
+        } else {
+          headerRef.current.style.opacity = 1;
+        }
+      });
+    });
+    observer.observe(titleRef.current);
+  }, []);
+
   return (
     <div className={styles.heroWrapper}>
+      <div
+        ref={headerRef}
+        className="opacity-0 transition ease-out fixed h-4 text-xs z-20 w-full text-center border-b shadow-sm bg-gray-100"
+      >
+        2024년도 임직감사예배 - 평내중앙교회
+      </div>
       <div className="hero-inner p-8 relative bg-gray-50 w-full h-[1000px]">
-        <div className={styles.title}>
+        <div ref={titleRef} className={styles.title}>
           <h1>임직감사예배</h1>
           <p>2024.09.29 주일 오후 2시</p>
           <p>평내중앙교회 3층 본당</p>
