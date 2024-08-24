15446f8c84ad5b24f86bae7a85c7025406e4fa24
namja85 <namja85@gmail.com>
2024-08-24 12:54:17 +0900

    description 변경

--- components/invitation/Footer.jsx
+++ components/invitation/Footer.jsx
@@ -27,8 +27,8 @@ export default function Footer() {
       Kakao.Share.sendDefault({
         objectType: 'feed',
         content: {
-          title: '2024년도 임직감사예배에 초대합니다.',
-          description: '⛪️ 평내중앙교회\n📅 2024.9.29 주일 오후2시',
+          title: '⛪️ 2024년도 임직감사예배에 초대합니다.',
+          description: '평내중앙교회 3층 본당\n2024.9.29 주일 오후2시',
           imageUrl: 'https://pnja.or.kr/share-thumbnail.jpg',
           imageWidth: 600,
           imageHeight: 938,
